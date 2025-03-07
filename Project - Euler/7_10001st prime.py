766088132c041857fccd7492e0c83994087ba437
Ida Aronsson <Ida.aronsson06@skola.stenungsund.se>
2025-03-07 14:39:20 +0100

    klar

--- Project - Euler/7_10001st prime.py
+++ Project - Euler/7_10001st prime.py
@@ -1,11 +1,17 @@
-def primtal(n):
-    if n < 2: # primtal är större än 1
-        return False
-    if n == 2: # 2 är det enda jämna primtalet
-        return False
-    if n % 2 == 0: # Alla jämna tal är inte primtal
-        return False
-        for i in range (3, int(n**0.5)+ 1, 2):
-            if n % i == 0:
-                return False
+primtalslista = [2] #lista för att lagra primtal, börjar med 2
+n = 100001 # primtalet vi vill hitta
+antal_primtal = 1 # räknar hur många primtal vi hittat
+tal = 3 # Börjar från 3 och letar efter primtal
+while antal_primtal < n:
+    är_primtal = True
+    for primtal in primtalslista:
+        if tal % primtal == 0:
+            är_primtal = False
+    if är_primtal:
+        primtalslista.append(tal) # lägg till primtalet i listan
+        antal_primtal += 1 
+    tal += 2 
+print("Det 10001:a primtalet är:", primtalslista[-1])
+
+
         
---
+++ Project - Euler/Fibbonacci_tal_25.py
@@ -0,0 +1,9 @@
+def fib(n):
+    F1, F2 = 1, 1 # de två första fib talen
+    n = 2 # n håller ordning på fib sekvensen, vi börjar med andra termen därav n = 2
+    while len(str(F2))<1000:# while loop som fortsätter tills längden av F2 (antal siffror) är minst 1000. Omvandla till sträng. Räknar siffrorna i F2. 
+        F1, F2 = F2, F1 + F2 # beräkna nästa fib tal med formeln given i frågan
+        n += 1 # vi ökar (n) eftersom vi går vidare till nästa fib tal 
+    return n # När F2 har 1000 siffror, retunerar man index n, alltsp vilken term som når 1000 siffror först. 
+print(fib(1000))
+
